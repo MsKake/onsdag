f5815a39911e9e170c2e2c3d7525dc5aa3d6f89b
MsKake <112157748+MsKake@users.noreply.github.com>
2022-11-17 10:17:55 +0100

    Add files via upload

---
+++ onsdag opgave/d/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d
+{
+    internal class Program
+    {
+     static int countDigit(long n)
+        {
+            int count = 0;
+            while (n != 0)
+            {
+                n = n / 10;
+                ++count;
+            }
+            return count;
+        }
+
+        /* Driver code */
+        public static void Main()
+        {
+            long n = 420691337222;
+            Console.WriteLine("Number of"
+                              + " digits : " + countDigit(n));
+        }
+    }
+}
